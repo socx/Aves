7c9d1095503e420c1a2de08004817402b42aa11f
socx <socx9@yahoo.com>
2017-03-07 22:14:51 +0000

    State after fixing pelican bug

--- Frontend/index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-header( 'Location: http://report.thehaloworks.com/halo/pages/client-login.html' );
-?>
--- Frontend/js/common/config.js
+++ Frontend/js/common/config.js
@@ -3,12 +3,13 @@ var Config = function () {
     return {
         //main function to initiate the module
         getAppRootUrl: function () {
-            //return '..';
-            //return 'http://restapp.thehaloworks.com/halo';
-            return 'http://roastthatmeat.co.uk/halo';
+            return 'http://guardiance.thehaloworks.com:8080/halo_reports';
+        },
+        getAppRootUrl1: function () {
+            return 'http://report.thehaloworks.com/frontend';
         },
         getApiRootUrl: function () {
-            return 'http://localhost:63547/api';
+            return 'http://report.thehaloworks.com/api';
         },
         getLoginPageUrl: function () {
             return 'http://report.thehaloworks.com/frontend/pages/client-login.html';
